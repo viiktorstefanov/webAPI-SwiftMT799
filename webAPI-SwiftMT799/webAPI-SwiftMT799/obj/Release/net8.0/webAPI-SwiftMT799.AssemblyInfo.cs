1b6f53dd98efbc168a23c710e41fa9833c2fcd31
Viktor Stefanov <victorystefanov@gmail.com>
2024-07-28 10:09:00 +0300

    splitting msg in 4 parts, README.md added

--- webAPI-SwiftMT799/webAPI-SwiftMT799/obj/Release/net8.0/webAPI-SwiftMT799.AssemblyInfo.cs
+++ webAPI-SwiftMT799/webAPI-SwiftMT799/obj/Release/net8.0/webAPI-SwiftMT799.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("webAPI-SwiftMT799")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a79abf6759942c2672a030de3b38e272393c3a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("webAPI-SwiftMT799")]
